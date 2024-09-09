40179c6f9788c72d5fb99cb49c6e1dad712407b2
Christoph Held <christoph11.held@nttdata.com>
2024-09-09 11:44:12 +0000

    Part2: Write sample event handler

--- srv/service.ts
+++ srv/service.ts
@@ -1,5 +1,17 @@
 import * as cds from '@sap/cds';
+import type {Composer} from '#cds-models/music';
 
 module.exports = class MusicService extends cds.ApplicationService { async init() {
+    const {Composers, Pieces} = await import('#cds-models/MusicService');
+
+    this.after('READ', Pieces, async (data) => {
+        for(let piece of data) {
+            const composer = await SELECT.from(Composers, piece.composer_ID) as Composer;
+            if(piece.publishdate > composer.dateOfdeath) {
+                piece.name += ' (posthumous)';
+            }
+        }
+    });
+
     return super.init()
 }}
